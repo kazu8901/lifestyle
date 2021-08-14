cd1979f9572080edd5888d1899a88afb64c64e17
kazu8901 <ka_8901@docomo.ne.jp>
2021-08-14 18:16:02 +0900

    マイグレーション終了

--- src/lifestyle/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/lifestyle/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('user_name', 30);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_icon')->nullable();
+            $table->text('intro')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
